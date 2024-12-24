1da5a92723dbbdcf57396d5a16e19fb4bb205db8
Lithasa <lithasa0417@gmail.com>
2024-12-24 11:37:45 +0530

    Added day 1 practicals

---
+++ Java_day1/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+       System.out.println(add(5,7));
+
+
+    }
+
+    public static int add(int a , int b){
+        int sum;
+        sum = a+b;
+        return sum;
+    }
+}
