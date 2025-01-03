5d35ad6d99a4978adc8b14df7b8b4af6744bfe82
Lithasa <lithasa0417@gmail.com>
2025-01-03 15:03:39 +0530

    Added a new practical

---
+++ Java_day4/src/Library.java
@@ -0,0 +1,37 @@
+
+public class Library {
+    
+    private  Book[] books;
+    private  User[] users;
+    
+    public Library(){
+        books = new Book[20];
+        users = new User[20];
+    }
+    
+    public void addBook(Book b1){
+        for(int i = 0; i< books.length; i++){
+            if(books[i]== null){
+                books[i] = b1;
+                return;
+            }
+        }
+        System.out.println("Library is full");
+        
+    }
+    public void addUser(User u1){
+        for(int i = 0; i< users.length; i++){
+            if(users[i]== null){
+                users[i] = u1;
+                return;
+            }
+        }
+        System.out.println("Library is full");
+        
+    }
+    
+    
+    
+    
+    
+}
---
+++ Java_day4/src/day4/Day4.java
@@ -0,0 +1,12 @@
+
+package day4;
+
+public class Day4 {
+
+   
+    public static void main(String[] args) {
+        
+        // TODO code application logic here
+    }
+    
+}
