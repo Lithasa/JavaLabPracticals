ce202fa061899f7124b8b6577e6dce6e5c3d81f4
Lithasa <lithasa0417@gmail.com>
2025-01-09 17:46:25 +0530

    updated

--- Java_day4/src/Book.java
+++ Java_day4/src/Book.java
@@ -25,19 +25,19 @@ public class Book {
     public String getTitle(){
         return title;
     }
-    public void setTitle(){
+    public void setTitle(String title){
         this.title = title;
     }
     public String getAuthor(){
         return author;
     }
-    public void setAuthor(){
+    public void setAuthor(String author){
         this.author = author;
     }
     public int getPrice(){
         return price;
     }
-    public void setPrice(){
+    public void setPrice(int price){
         this.price = price;
     }
     public int getStockQuantity(){
