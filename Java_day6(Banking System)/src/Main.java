c105c2311ac072ad11a7454ee34546fc968dd7d7
Lithasa <lithasa0417@gmail.com>
2025-01-18 07:43:32 +0530

    tested the code

--- Java_day6(Banking System)/src/Main.java
+++ Java_day6(Banking System)/src/Main.java
@@ -6,6 +6,15 @@ public class Main {
         BankAccount acc1 = new BankAccount();
         acc1.depositFunds(1000);
         acc1.getBalance();
+
+        SavingsAccount acc2 = new SavingsAccount();
+        acc2.depositFunds(10000);
+        acc2.depositFunds(2500);
+        acc2.getBalance();
+        acc2.setInterestRate(20);
+        acc2.getInterestRate();
+
+
     }
     
 }
